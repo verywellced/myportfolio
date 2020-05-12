53cb43f81d745a4c256616d8a3f49314d7a58766
verywellced <cedrick.pablo71@gmail.com>
2020-05-12 15:59:09 +0800

    up

--- src/app/elements/carousel/carousel.component.ts
+++ src/app/elements/carousel/carousel.component.ts
@@ -9,7 +9,7 @@ import { AboutInfo } from './about.model';
 export class CarouselComponent implements OnInit {
   aboutInfo: AboutInfo[] = [
     {
-      imgString: "../../../assets/images/pic1.jpg",
+      imgString: "./assets/images/pic1.jpg",
       categories: 'Education',
       info: " In 2019, I graduated from Bulacan State University in Malolos, Bulacan with the degree of B.S. Computer Engineering. It was very memorable college life. "
       + "As an Eng'g students there are so many things that I have to accomplished way back then." + "\n \n " +
@@ -18,7 +18,7 @@ export class CarouselComponent implements OnInit {
       active: "active"
     },
     {
-      imgString: "../../../assets/images/pic2.jpg",
+      imgString: "./assets/images/pic2.jpg",
       categories: 'Affiliations',
       info: "I'm currently serving the Lord as a Youth Leader at our church. On this aspect of my life, I've learned how to serve others and what it means to be a servant of all. Also I've learned so much on how to be a good team player and to be focus in order to achieve your goal."
       + "\n \n" +
@@ -27,14 +27,14 @@ export class CarouselComponent implements OnInit {
       active: null,
     },
     {
-      imgString: "../../../assets/images/pic3.jpg",
+      imgString: "./assets/images/pic3.jpg",
       categories: 'Interest',
       info: "I love music. My hobby is to worship the Lord with my guitar. Even though my voice is not that good, I've focus on making music through strings." +
        "Im also a drummer and one of the values I've learn through this instruments is to not give up when you desired something." +"\n \n" + "Learning instruments is not that easy and you can make excuses to stop but as a qoute says 'Don't make your problems as an excuse.",
       active: null,
     },
     {
-      imgString: "../../../assets/images/pic4.jpg",
+      imgString: "./assets/images/pic4.jpg",
       categories: 'Career',
       info: "After I graduated, I worked as a Jr. IT Developer at Withcenter Inc. on Angeles, Pampanga." +
       "Before I get hired, the IT Head tells me to submit a certain project that requires HTML, CSS and JAVASCRIPT knowledge." + "\n \n" +
