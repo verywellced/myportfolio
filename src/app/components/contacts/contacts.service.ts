4a81eb9077a47115f834b0d6aaae5d413955be0b
verywellced <cedrick.pablo71@gmail.com>
2020-05-12 16:56:43 +0800

    up

--- src/app/components/contacts/contacts.service.ts
+++ src/app/components/contacts/contacts.service.ts
@@ -8,25 +8,25 @@ export class ContactsService {
   private _contacts: Contact[] = [
     {
       title: 'Facebook',
-      imgSrc: './../assets/images/facebook.png',
+      imgSrc: './assets/images/facebook.png',
       info: 'facebook.com/verywellced',
       link: 'https://www.facebook.com/verywellced',
   },
       {
         title: 'Linkedin',
-        imgSrc: './../assets/images/linkin.png',
+        imgSrc: './assets/images/linkin.png',
         info: 'linkedin.com/in/verywellced/',
         link: 'https://www.linkedin.com/in/verywellced/',
       },
       {
         title: 'Twitter',
-        imgSrc: './../assets/images/twitter.png',
+        imgSrc: './assets/images/twitter.png',
         info: 'twitter.com/verywellced17',
         link: 'https://twitter.com/verywellced17',
       },
       {
         title: 'Gmail',
-        imgSrc: './../assets/images/gmail.png',
+        imgSrc: './assets/images/gmail.png',
         info: 'cedrick.pablo71@gmail.com',
         link: 'mailto:cedrick.pablo71@gmail.com',
     },
