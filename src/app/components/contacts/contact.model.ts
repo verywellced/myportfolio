b880a0ec71a1dbd8e4d8714fe56855f12fdabc5f
verywellced <cedrick.pablo71@gmail.com>
2020-05-12 17:40:06 +0800

    Auto-generated commit

--- src/app/components/contacts/contact.model.ts
+++
@@ -1,8 +0,0 @@
-export class Contact {
-  constructor(
-    public title: string,
-    public info: string,
-    public imgSrc: string,
-    public link: string,
-  ) {}
-}
--- src/app/elements/carousel/about.model.ts
+++
@@ -1,8 +0,0 @@
-export class AboutInfo {
-  constructor(
-    public imgString: String,
-    public categories: String,
-    public info: String,
-    public active: String,
-  ){}
-}
